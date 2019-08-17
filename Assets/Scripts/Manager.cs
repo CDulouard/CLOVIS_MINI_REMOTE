28840aac357ad5a37fa92ee40f42ddd35f87421e
CDulouard <clement.dulouard@gmail.com>
2019-08-17 19:51:33 +0200

    fix thread collisions on _values

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
-    public InputField localIP;
+    public InputField localIp;
     public InputField localPort;
-    public InputField serverIP;
+    public InputField serverIp;
     public InputField serverPort;
     public InputField serverPassword;
     public Text errorDisplayer;
@@ -27,7 +28,10 @@ public class Manager : MonoBehaviour
         connectButton.onClick.AddListener(Connection);
         foreach (var selecter in selecters)
         {
-            _values.Add(selecter.selecterTag, int.Parse(selecter.textInput.text));
+            lock (_values)
+            {
+                _values.Add(selecter.selecterTag, int.Parse(selecter.textInput.text));
+            }
         }
         var dataRefresh = new Thread(SendData);
         dataRefresh.Start();
@@ -68,8 +72,8 @@ public class Manager : MonoBehaviour
 
     private void Connection()
     {
-        _socket.Start(localIP.text, int.Parse(localPort.text));
-        _socket.ConnectionToServer(serverIP.text, int.Parse(serverPort.text), serverPassword.text);
+        _socket.Start(localIp.text, int.Parse(localPort.text));
+        _socket.ConnectionToServer(serverIp.text, int.Parse(serverPort.text), serverPassword.text);
     }
 
     private void SendData()
@@ -97,16 +101,3 @@ public class Manager : MonoBehaviour
         return json;
     }
 }
-/*
-InvalidOperationException: Collection was modified; enumeration operation may not execute.
-System.Collections.Generic.Dictionary`2+Enumerator[TKey,TValue].MoveNext () (at <a8ed250850854b439cedc18931a314fe>:0)
-Manager.ValuesToJson () (at D:/Clement/Documents/Unity/Remote/Assets/Scripts/Manager.cs:86)
-Manager.SendData () (at D:/Clement/Documents/Unity/Remote/Assets/Scripts/Manager.cs:78)
-System.Threading.ThreadHelper.ThreadStart_Context (System.Object state) (at <a8ed250850854b439cedc18931a314fe>:0)
-System.Threading.ExecutionContext.RunInternal (System.Threading.ExecutionContext executionContext, System.Threading.ContextCallback callback, System.Object state, System.Boolean preserveSyncCtx) (at <a8ed250850854b439cedc18931a314fe>:0)
-System.Threading.ExecutionContext.Run (System.Threading.ExecutionContext executionContext, System.Threading.ContextCallback callback, System.Object state, System.Boolean preserveSyncCtx) (at <a8ed250850854b439cedc18931a314fe>:0)
-System.Threading.ExecutionContext.Run (System.Threading.ExecutionContext executionContext, System.Threading.ContextCallback callback, System.Object state) (at <a8ed250850854b439cedc18931a314fe>:0)
-System.Threading.ThreadHelper.ThreadStart () (at <a8ed250850854b439cedc18931a314fe>:0)
-UnityEngine.UnhandledExceptionHandler:<RegisterUECatcher>m__0(Object, UnhandledExceptionEventArgs)
-
-*/
