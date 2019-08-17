ebffa3e83325322af48e1a79040765f1a37b8992
CDulouard <clement.dulouard@gmail.com>
2019-08-18 00:03:03 +0200

    add compiled files

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
